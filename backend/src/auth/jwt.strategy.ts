6b8834de677420b8dcfbfed586e28b81fbccdff2
Muhammad Iqbal Pranatayudha <pranatayudha001@gmail.com>
2024-04-17 23:01:47 +0700

    feat(jwt-strategy): Add 'validate' method for get user data from bearer token

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -1,14 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private readonly usersService: UsersService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: 'linkz-assesment-api',
     });
+  }
+
+  async validate(payload: any): Promise<any> {
+    const user = await this.usersService.getUser(payload);
+
+    if (!user) {
+      throw new ForbiddenException('Invalid Token!');
+    }
+
+    return user;
   }
 }
